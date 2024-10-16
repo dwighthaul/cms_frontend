8ff46cb9bd6971b08f1e13b584788e47d4dc75d9
Paul Hubert <paulohubert.ph@hotmail.fr>
2024-10-16 01:33:18 -0400

    Update url

--- src/app/shared/service/backend/callbackend.service.ts
+++ src/app/shared/service/backend/callbackend.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 export class CallbackendService {
 
   // @TODO : Recuperer depuis la configuration
-  public baseUrl = 'http://3.217.171.7:3000'; // URL de base de l'API
+  public baseUrl = 'https://3.217.171.7:3000'; // URL de base de l'API
 
   constructor(private http: HttpClient) { }
 
