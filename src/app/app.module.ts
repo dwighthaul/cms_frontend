a760692dde420a426116eba54e1c690a4d58a0b3
Paul Hubert <paulohubert.ph@hotmail.fr>
2024-10-29 12:12:44 +0100

    Update de la page d'edition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,8 +18,39 @@ import { TempComponent } from './routes/temp/temp.component';
 import { UsersPagesComponent } from './routes/users-pages/users-pages.component';
 import { AdminMenuComponent } from './shared/admin-menu/admin-menu.component';
 import { PageDisplayComponent } from './shared/components/page-display/page-display.component';
-import { PageEditorComponent } from './shared/components/page-editor/page-editor.component';
 import { PageEditeurPageComponent } from './shared/components/page-editeur-page/page-editeur-page.component';
+import { PageEditorComponent } from './shared/components/page-editor/page-editor.component';
+
+
+
+var toolbar = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+  [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  //[{ 'color': ["#998888"] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean'],                                         // remove formatting button
+
+  ['link', 'image', 'video']                         // link and image, video
+]
+
+console.log("--------------------------")
+QuillModule.forRoot().providers?.every((e) => {
+  console.log(e.toLocaleString)
+})
+console.log("--------------------------")
+
 
 
 @NgModule({
@@ -45,7 +76,11 @@ import { PageEditeurPageComponent } from './shared/components/page-editeur-page/
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    QuillModule.forRoot()
+    QuillModule.forRoot({
+      modules: {
+        //toolbar: toolbar
+      }
+    })
 
   ],
   providers: [],
--- src/app/shared/components/page-editor/page-editor.component.ts
+++ src/app/shared/components/page-editor/page-editor.component.ts
@@ -1,79 +1,91 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-page-editor',
-  templateUrl: './page-editor.component.html',
-  styleUrls: ['./page-editor.component.css']
+	selector: 'app-page-editor',
+	templateUrl: './page-editor.component.html',
+	styleUrls: ['./page-editor.component.css']
 })
 export class PageEditorComponent {
 
-  types = [{
-    value: "images",
-    name: "Images",
-    format: {
-      images: []
-    }
-  }, {
-    value: "textimage",
-    name: "Texte + Image",
-    format: {
-      contenu: "",
-      image: ""
-    }
-  },
-  {
-    value: "text",
-    name: "Text",
-    format: {
-      contenu: ""
-    }
-  },
-  {
-    value: "separator",
-    name: "Separateur",
-    format: {
-      contenu: ""
-    }
-  },
-  ]
-
-  ajouterUneImage(contenu: any) {
-    contenu.images.push({ src: "" });
-  }
-
-  typePageAAjouter: string | undefined;
+	modules = {
+		toolbar: [
+			[{ font: [] }, { size: [] }],
+			['bold', 'italic', 'underline', 'strike'],
+			[{ color: ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466"] }, { background: [] }],
+			[{ list: 'ordered' }, { list: 'bullet' }],
+			[{ align: [] }],
+			['link', 'image', 'video']
+		]
+	};
 
 
-  @Input() page: any;
+	types = [{
+		value: "images",
+		name: "Images",
+		format: {
+			images: []
+		}
+	}, {
+		value: "textimage",
+		name: "Texte + Image",
+		format: {
+			contenu: "",
+			image: ""
+		}
+	},
+	{
+		value: "text",
+		name: "Text",
+		format: {
+			contenu: ""
+		}
+	},
+	{
+		value: "separator",
+		name: "Separateur",
+		format: {
+			contenu: ""
+		}
+	},
+	]
+
+	ajouterUneImage(contenu: any) {
+		contenu.images.push({ src: "" });
+	}
+
+	typePageAAjouter: string | undefined;
 
 
-  ajouterUnParagraphe() {
-
-    var type = this.types.find((i) => { return (this.typePageAAjouter != undefined && (i.value == this.typePageAAjouter)) })
-
-    if (!type) {
-      return
-    }
-
-    this.page.contenu.push({
-      type: this.typePageAAjouter,
-      ...type?.format
-
-    });
-  }
-
-  // TODO : Supprimer
-  supprimerParagraphe(content: any) {
-    const index = this.page.contenu.indexOf(content);
-    this.page.contenu.splice(index, 1);
-  }
-
-  supprimerLien(contenu: any, image: any) {
-    const index = contenu.images.indexOf(image);
-    contenu.images.splice(index, 1);
+	@Input() page: any;
 
 
-  }
+	ajouterUnParagraphe() {
+
+		var type = this.types.find((i) => { return (this.typePageAAjouter != undefined && (i.value == this.typePageAAjouter)) })
+
+		if (!type) {
+			return
+		}
+
+		this.page.contenu.push({
+			type: this.typePageAAjouter,
+			...type?.format
+
+		});
+	}
+
+	// TODO : Supprimer
+	supprimerParagraphe(content: any) {
+		const index = this.page.contenu.indexOf(content);
+		this.page.contenu.splice(index, 1);
+	}
+
+	supprimerLien(contenu: any, image: any) {
+		const index = contenu.images.indexOf(image);
+		contenu.images.splice(index, 1);
+
+
+	}
 
 
 }
